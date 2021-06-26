d686759b009ab990f8c0abf0387d7bdad4c7b072
Ryan Aunur <ryandevstudio@gmail.com>
2021-06-26 15:59:34 +0700

    components(color-input): add error indicator

--- src/components/ColorConvert/ColorInput.vue
+++ src/components/ColorConvert/ColorInput.vue
@@ -2,15 +2,24 @@
   <div class="section input-wrapper">
     <div class="label">
       <span class="heading">{{ type.title }}:</span>
-      <div class="icon" :class="`icon-copy--${color}`" @click="onCopyClick" title="Copy color to clipboard"></div>
+      <div
+        class="icon"
+        :class="`icon-copy--${color}`"
+        @click="onCopyClick"
+        title="Copy color to clipboard"
+      ></div>
     </div>
-    <div class="content color-input" :class="type.id" :style="`color: ${hex}`">
+    <div
+      class="content color-input"
+      :class="{ error: isError, [type.id]: true }"
+      :style="`color: ${hex}`"
+    >
       <template v-if="property.inputType == 'number'">
         <input
           v-for="n in property.inputLength"
           type="number"
           name="values[]"
-          v-model.number="values[n-1]"
+          v-model.number="values[n - 1]"
           :maxlength="property.inputMaxLength"
           :key="`input-${n}`"
           :id="`${type.id}-input-${n}`"
@@ -22,14 +31,13 @@
           v-for="n in property.inputLength"
           type="text"
           name="values[]"
-          v-model="values[n-1]"
+          v-model="values[n - 1]"
           :maxlength="property.inputMaxLength"
           :key="`input-${n}`"
           :id="`${type.id}-input-${n}`"
           @input="onInputChanged"
         />
       </template>
-      
     </div>
   </div>
 </template>
@@ -46,25 +54,26 @@ export default {
     hex: String,
     color: String,
   },
-  data () {
+  data() {
     return {
-      values: []
-    }
+      isError: false,
+      values: [],
+    };
   },
-  mounted () {
+  mounted() {
     this.onInitialized();
   },
   computed: {
-    property () {
+    property() {
       return getColorProperties(this.type.id);
-    }
+    },
   },
   methods: {
-    onInitialized () {
+    onInitialized() {
       if (this.type.id == "hex") {
-        this.values = [ this.type.value ];
+        this.values = [this.type.value];
       } else {
-        this.values = [ ...this.type.value ];
+        this.values = [...this.type.value];
       }
     },
     onCopyClick() {
@@ -75,37 +84,118 @@ export default {
       this.notyf.success("Color copied to clipboard!");
     },
     onInputChanged($event) {
-      let color = [ ...this.values ];
+      let color = [...this.values];
       let isColorValid = colorValidate(this.type.id, color);
 
       if (this.type.id == "hex") {
         color = color[0];
       }
 
-      this.$emit("changed", [ this.type.id, color ])
+      if (isColorValid) {
+        this.isError = false;
+        this.$emit("colorChanged", [this.type.id, color]);
+      } else {
+        this.isError = true;
+      }
     },
   },
   watch: {
-    hex () {
+    hex() {
       this.onInitialized();
     },
-    type () {
+    type() {
       this.onInitialized();
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style lang="scss">
 .input-wrapper {
-  .icon {
-    cursor: pointer;
-  }
-
-  > .label {
+  .label {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin-bottom: 8px;
+
+    > .heading {
+      display: inline-block;
+      width: calc(100% - 24px);
+    }
+
+    .icon {
+      cursor: pointer;
+    }
+    .hidden-label {
+      display: none;
+    }
+  }
+
+  .color-input {
+    position: relative;
+    width: 100%;
+
+    &.error input {
+      border-inline: 3px solid #d32f2f;
+    }
+
+    input {
+      font-family: "IBM Plex Mono", monospace;
+      letter-spacing: 0.4em;
+
+      -webkit-appearance: none;
+      border: 0;
+      outline: none;
+      background: #ffffff;
+      padding: 20px 0;
+      border-radius: 3px;
+      text-align: center;
+
+      box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.14),
+        0 2px 1px -1px rgba(0, 0, 0, 0.12), 0 1px 3px 0 rgba(0, 0, 0, 0.2);
+      font-size: 1.5rem;
+      font-weight: bold;
+
+      color: #212121;
+      border-inline: 3px solid transparent;
+      transition: border-inline 0.5s cubic-bezier(0.215, 0.61, 0.355, 1);
+    }
+
+    &.hex {
+      > input {
+        width: calc(100% - 26px);
+        padding-left: 1em;
+      }
+
+      &:before {
+        content: "#";
+        position: absolute;
+        color: #9e9e9e;
+        top: 20px;
+        left: 20px;
+        font-family: monospace;
+        font-size: 2em;
+      }
+    }
+
+    &.rgb input,
+    &.hsl input {
+      width: calc((100% / 3) - 6px);
+      margin-right: 8px;
+
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+
+    &.cmyk input {
+      width: calc(25% - 6px);
+      margin-right: 8px;
+
+      &:last-child {
+        margin-right: 0;
+      }
+    }
   }
 }
 </style>
