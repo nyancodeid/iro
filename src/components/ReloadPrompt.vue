4318d6b2bed0d4d8ab811d59fd322e6c55310e2b
Ryan Aunur <ryandevstudio@gmail.com>
2021-06-26 16:28:17 +0700

    component(pwa-reload): add dynamic style

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -22,7 +22,7 @@ const close = async () => {
 };
 </script>
 
-<style>
+<style lang="scss">
 .pwa-toast {
   position: fixed;
   right: 0;
@@ -34,15 +34,28 @@ const close = async () => {
   z-index: 1;
   text-align: left;
   box-shadow: 3px 4px 5px 0px #8885;
-}
-.pwa-toast .message {
-  margin-bottom: 8px;
-}
-.pwa-toast button {
-  border: 1px solid #8885;
-  outline: none;
-  margin-right: 5px;
-  border-radius: 2px;
-  padding: 3px 10px;
+  background-color: var(--dark-color);
+  color: var(--secondary-color);
+
+  .message {
+    margin-bottom: 8px;
+  }
+  button {
+    margin-right: 5px;
+    box-shadow: 0 1px 1px 0 rgb(0 0 0 / 14%), 0 2px 1px -1px rgb(0 0 0 / 12%),
+      0 1px 3px 0 rgb(0 0 0 / 20%);
+    background: var(--secondary-color);
+    color: var(--text-color);
+    border-radius: 2px;
+    text-align: center;
+    font-weight: bold;
+    font-size: 14px;
+    cursor: pointer;
+    outline: none;
+    border: none;
+    padding: 11px 16px;
+    display: inline-block;
+    width: 120px;
+  }
 }
 </style>
