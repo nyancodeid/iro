bbefc899a6f49862e523bc0a0698af29496171a3
Ryan Aunur <ryandevstudio@gmail.com>
2021-06-26 17:07:17 +0700

    pwa(update): fix icons and pwa manifest

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,25 @@ export default defineConfig({
         name: "IRO - Amazing Color Tools",
         shortName: "IRO",
         theme_color: "#212121",
-      }
-    })
+        includeAssets: ["/favicon.ico"],
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
 });
