793e2e8421013b1912feefc09f2a07a82675a395
Ryan Aunur <ryandevstudio@gmail.com>
2021-07-10 20:32:40 +0700

    services(convertes): fix hsl convert error

--- src/services/converter.js
+++ src/services/converter.js
@@ -3,58 +3,61 @@
  **/
 
 export const hex = {
-	toRgb (args) {
-		const match = args.toString(16).match(/[a-f0-9]{6}|[a-f0-9]{3}/i);
-		if (!match) {
-			return [0, 0, 0];
-		}
+  toRgb(args) {
+    const match = args.toString(16).match(/[a-f0-9]{6}|[a-f0-9]{3}/i);
+    if (!match) {
+      return [0, 0, 0];
+    }
 
-		let colorString = match[0];
+    let colorString = match[0];
 
-		if (match[0].length === 3) {
-			colorString = colorString.split('').map(char => {
-				return char + char;
-			}).join('');
-		}
+    if (match[0].length === 3) {
+      colorString = colorString
+        .split("")
+        .map((char) => {
+          return char + char;
+        })
+        .join("");
+    }
 
-		const integer = parseInt(colorString, 16);
-		const r = (integer >> 16) & 0xFF;
-		const g = (integer >> 8) & 0xFF;
-		const b = integer & 0xFF;
+    const integer = parseInt(colorString, 16);
+    const r = (integer >> 16) & 0xff;
+    const g = (integer >> 8) & 0xff;
+    const b = integer & 0xff;
 
-		return [r, g, b];
-	},
-	toHsl (args) {
-		const _rgb = hex.toRgb(args);
-		return rgb.toHsl(_rgb);
-	},
-	toCmyk (args) {
-		const rgb = hex.toRgb(args);
+    return [r, g, b];
+  },
+  toHsl(args) {
+    const _rgb = hex.toRgb(args);
+    return rgb.toHsl(_rgb);
+  },
+  toCmyk(args) {
+    const rgb = hex.toRgb(args);
 
-		const r = rgb[0] / 255;
-		const g = rgb[1] / 255;
-		const b = rgb[2] / 255;
+    const r = rgb[0] / 255;
+    const g = rgb[1] / 255;
+    const b = rgb[2] / 255;
 
-		const k = Math.min(1 - r, 1 - g, 1 - b);
-		const c = (1 - r - k) / (1 - k) || 0;
-		const m = (1 - g - k) / (1 - k) || 0;
-		const y = (1 - b - k) / (1 - k) || 0;
+    const k = Math.min(1 - r, 1 - g, 1 - b);
+    const c = (1 - r - k) / (1 - k) || 0;
+    const m = (1 - g - k) / (1 - k) || 0;
+    const y = (1 - b - k) / (1 - k) || 0;
 
-		return [c * 100, m * 100, y * 100, k * 100];
-	}
-}
+    return [c * 100, m * 100, y * 100, k * 100];
+  },
+};
 
 export const rgb = {
-	toHex (args) {
-		const integer = ((Math.round(args[0]) & 0xFF) << 16) +
-      ((Math.round(args[1]) & 0xFF) << 8) +
-      (Math.round(args[2]) & 0xFF);
+  toHex(args) {
+    const integer =
+      ((Math.round(args[0]) & 0xff) << 16) +
+      ((Math.round(args[1]) & 0xff) << 8) +
+      (Math.round(args[2]) & 0xff);
 
-    const string = integer.toString(16)
-      .toUpperCase();
-    return '000000'.substring(string.length) + string;
-	},
-	toHsl (args) {
+    const string = integer.toString(16).toUpperCase();
+    return "000000".substring(string.length) + string;
+  },
+  toHsl(args) {
     const r = args[0] / 255;
     const g = args[1] / 255;
     const b = args[2] / 255;
@@ -65,34 +68,34 @@ export const rgb = {
     let s;
 
     if (max === min) {
-        h = 0;
+      h = 0;
     } else if (r === max) {
-        h = (g - b) / delta;
+      h = (g - b) / delta;
     } else if (g === max) {
-        h = 2 + (b - r) / delta;
+      h = 2 + (b - r) / delta;
     } else if (b === max) {
-        h = 4 + (r - g) / delta;
+      h = 4 + (r - g) / delta;
     }
 
     h = Math.min(h * 60, 360);
 
     if (h < 0) {
-        h += 360;
+      h += 360;
     }
 
     const l = (min + max) / 2;
 
     if (max === min) {
-        s = 0;
+      s = 0;
     } else if (l <= 0.5) {
-        s = delta / (max + min);
+      s = delta / (max + min);
     } else {
-        s = delta / (2 - max - min);
+      s = delta / (2 - max - min);
     }
 
     return [h, s * 100, l * 100];
-	},
-	toCmyk (args) {
+  },
+  toCmyk(args) {
     const r = args[0] / 255;
     const g = args[1] / 255;
     const b = args[2] / 255;
@@ -103,15 +106,15 @@ export const rgb = {
     const y = (1 - b - k) / (1 - k) || 0;
 
     return [c * 100, m * 100, y * 100, k * 100];
-	}
-}
+  },
+};
 
 export const hsl = {
-  toHex (args) {
-    const _rgb = hex.toRgb(args);
+  toHex(args) {
+    const _rgb = hsl.toRgb(args);
     return rgb.toHex(_rgb);
   },
-  toRgb (args) {
+  toRgb(args) {
     const h = args[0] / 360;
     const s = args[1] / 100;
     const l = args[2] / 100;
@@ -134,7 +137,7 @@ export const hsl = {
 
     const rgb = [0, 0, 0];
     for (let i = 0; i < 3; i++) {
-      t3 = h + 1 / 3 * -(i - 1);
+      t3 = h + (1 / 3) * -(i - 1);
       if (t3 < 0) {
         t3++;
       }
@@ -158,18 +161,18 @@ export const hsl = {
 
     return rgb;
   },
-  toCmyk (args) {
-    const _rgb = hex.toRgb(args);
+  toCmyk(args) {
+    const _rgb = hsl.toRgb(args);
     return rgb.toCmyk(_rgb);
-  }
-}
+  },
+};
 
 export const cmyk = {
-  toHex (args) {
+  toHex(args) {
     const _rgb = cmyk.toRgb(args);
     return rgb.toHex(_rgb);
   },
-  toRgb (args) {
+  toRgb(args) {
     const c = args[0] / 100;
     const m = args[1] / 100;
     const y = args[2] / 100;
@@ -181,8 +184,8 @@ export const cmyk = {
 
     return [r * 255, g * 255, b * 255];
   },
-  toHsl (args) {
+  toHsl(args) {
     const _rgb = cmyk.toRgb(args);
     return rgb.toHsl(_rgb);
-  }
-}
+  },
+};
