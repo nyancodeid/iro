9fb89dd05d39838ea78057feaeacb6de1f103208
Ryan Aunur <ryandevstudio@gmail.com>
2021-08-08 10:54:29 +0700

    improve(lighthouse): pwa score

--- vite.config.js
+++ vite.config.js
@@ -25,16 +25,19 @@ export default defineConfig({
             src: "/icons/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "any maskable"
           },
           {
             src: "/icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any maskable"
           },
           {
             src: "/icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any maskable"
           },
         ],
       },
