bef091bd18bd8c6a25558d9633657800ca9aa292
nyancodeid <ryandevstudio@gmail.com>
2021-08-15 10:57:37 +0700

    new(component/lazy): make component rendered laziness

---
+++ src/components/Lazy.vue
@@ -0,0 +1,19 @@
+<template>
+  <slot v-if="shouldRender" />
+</template>
+
+<script>
+import { ref, nextTick } from 'vue';
+export default {
+  name: "Lazy",
+  setup() {
+    const shouldRender = ref(false);
+
+    nextTick(() => {
+      shouldRender.value = true;
+    });
+
+    return { shouldRender };
+  }
+}
+</script>
